e9bdc2bba55690e4668d2e314f329b4108acf8ce
AryanV311 <159629630+AryanV311@users.noreply.github.com>
2024-10-03 16:05:53 +0530

    change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,19 @@
 // import React from 'react'
-import { useState } from "react"
-import { ExploreMenu } from "../../components/ExploreMenu"
-import { Header } from "../../components/Header"
-import "./Home.css"
-import { FoodDisplay } from "../../components/foodDisplay/foodDisplay"
-import { AppDownload } from "../../components/AppDowanload/AppDownload"
+import { useState } from "react";
+import { ExploreMenu } from "../../components/ExploreMenu";
+import { Header } from "../../components/Header";
+import "./Home.css";
+import { FoodDisplay } from "../../components/foodDisplay/foodDisplay";
+import { AppDownload } from "../../components/AppDowanload/AppDownload";
 
 export const Home = () => {
   const [category, setCategory] = useState("All");
   return (
     <div>
-        <Header/>
-        <ExploreMenu category={category} setCategory={setCategory} />
-        <FoodDisplay category={category}/>
-        <AppDownload />
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
+      <AppDownload />
     </div>
-  )
-}
+  );
+};
